6b8fc3b1f98cbd336b1d260c7848d536b9c3430c
heltutan <heltutan@kpmg.com>
2021-07-07 14:57:41 +0200

    Revert "Coplete basic configurations"
    This reverts commit 906c5d2f7b2feb8f590caf50861fcfbf2b1ff361.

--- app/app.ts
+++
@@ -1,6 +0,0 @@
-function startGame() {
-    var messagesElement = document.getElementById('messages');
-    messagesElement!.innerHTML = 'Welcome';
-}
-
-document.getElementById('startGame')!.addEventListener('click', startGame);
